de47674789f7f383e3f8e3a5734aa0e56a63bec0
Stanislav <verbatim8872@gmail.com>
2021-03-02 21:48:49 +0200

    fix key '0'

--- Keyboard/keyboard.js
+++ Keyboard/keyboard.js
@@ -41,7 +41,7 @@ const Keyboard = {
     _createKeys() {
         const fragment = document.createDocumentFragment();
         const keysLayout = [
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'backspace',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'backspace',
             'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
             'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'enter',
             'done', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '?',
